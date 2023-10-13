576a68d3beeec2c81c9fad65d49c59dc427520d9
phamtanphat <phatdroid94@gmail.com>
2023-10-13 21:10:38 +0700

    demo1

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Widget
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -7,12 +8,83 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// State Object
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My home page"),
+      ),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                        child: Container(
+                            color: Colors.red,
+                            child: Center(child: Text("A"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.blue,
+                            child: Center(child: Text("B"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.orange,
+                            child: Center(child: Text("C"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.teal,
+                            child: Center(child: Text("D"))
+                        )
+                    ),
+                  ],
+                )
+            ),
+            Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                        child: Container(
+                            color: Colors.red,
+                            child: Center(child: Text("A"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.blue,
+                            child: Center(child: Text("B"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.orange,
+                            child: Center(child: Text("C"))
+                        )
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.teal,
+                            child: Center(child: Text("D"))
+                        )
+                    ),
+                  ],
+                )
+            ),
+          ],
+        ),
       ),
     );
   }
